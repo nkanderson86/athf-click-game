e9fdbf717ac3c5949e8aac9b1bf577aa1e87df5a
nkanderson86 <N.ANDERSON.398@GMAIL.COM>
2019-05-05 17:41:11 -0700

    Minor change to instructions wording

--- athf-click-game/src/components/Title/index.jsx
+++ athf-click-game/src/components/Title/index.jsx
@@ -8,7 +8,7 @@ const Title = () => {
                 <div className='container'>
                     <div className='row'>
                         <h2 className='header col s12 light'>Clicky Game!</h2>
-                        <h5>Click on your favorite Aqua Teen Hunger Force characters to score points.  Click each character only once! If you can do that twelve times without forgetting, you win! </h5>
+                        <h5>Click on your favorite Aqua Teen Hunger Force characters to score points.  Click each character once! If you can do that twelve times without clicking on the same character twice, you win! </h5>
                     </div>
                 </div>
             </div>
